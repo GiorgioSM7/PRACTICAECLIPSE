0378cfd0042997d4d2c0bd0cd4887814275fa43a
Daw2 <jorgesalazar.daw@ciudadescolarfp.es>
2022-04-22 14:01:02 +0200

    Instancio desde la rama master

--- src/PracticaJorge.java
+++ src/PracticaJorge.java
@@ -5,6 +5,9 @@ public class PracticaJorge {
 		// TODO Auto-generated method stub
 System.out.println("Listado de alumnos 1o DAW");
 System.out.println("Alumno1");
+
+Alumno a = new Alumno("Jorge","Salazar","555");
+System.out.println(a);
 	}
 
 }
